df31498064d9a980e2012b7bc69deaa7f2b93ea2
Hoerudin <hoerudinpermana30@gmail.com>
2024-11-08 04:54:57 +0700

    belajar mix HTML

--- mix_HTML.php
+++ mix_HTML.php
@@ -1,4 +1,9 @@
 <?php
+$nama = "Hoerudin";
+$domisili = "Bogor";
+$jenisKelamin = "Laki-laki";
+
+echo "Selamat Pagi", "<h1>$nama</h1>";
 
 ?>
 
@@ -10,6 +15,8 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <h1>Selamat Datang, <?php echo $nama ?></h1>
+    <h2>Domisili: <?Php echo $domisili ?></h2>
+    <h2>Jenis Kelamin: <?php echo $jenisKelamin ?></h2>
 </body>
 </html>
