b40cbee42e316fc4f134b3e5d1c33debbe0ec8a8
Hoerudin <hoerudinpermana30@gmail.com>
2024-11-08 04:56:45 +0700

    belajar mix HTML

--- mix_HTML.php
+++ mix_HTML.php
@@ -3,7 +3,7 @@ $nama = "Hoerudin";
 $domisili = "Bogor";
 $jenisKelamin = "Laki-laki";
 
-echo "Selamat Pagi", "<h1>$nama</h1>";
+// echo "Selamat Pagi", "<h1>$nama</h1>";
 
 ?>
 
@@ -15,7 +15,8 @@ echo "Selamat Pagi", "<h1>$nama</h1>";
     <title>Document</title>
 </head>
 <body>
-    <h1>Selamat Datang, <?php echo $nama ?></h1>
+    <h1>Biodata</h1>
+    <h2>Nama: <?php echo $nama ?></h2>
     <h2>Domisili: <?Php echo $domisili ?></h2>
     <h2>Jenis Kelamin: <?php echo $jenisKelamin ?></h2>
 </body>
