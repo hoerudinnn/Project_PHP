c079db0b364209d4eed9cb97d36691f07e03297d
Hoerudin <hoerudinpermana30@gmail.com>
2024-11-08 10:21:58 +0700

    mengenal fungsi

---
+++ fungsi.php
@@ -0,0 +1,18 @@
+<?php
+$name = ucwords("hoerudin");
+
+echo strtoupper("budi gunawan");
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Selamat Datang, <?php echo $name?></h1>
+</body>
+</html>
