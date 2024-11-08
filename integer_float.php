8433c95fc378c96dd24b5e31f18d9792a7ed5932
Hoerudin <hoerudinpermana30@gmail.com>
2024-11-08 14:35:12 +0700

    belajar integer, float, increment dan decrement

---
+++ integer_float.php
@@ -0,0 +1,38 @@
+<?php
+$angka1 = 5; // ini adalah integer
+$angka2 = -10; // ini juga integer
+$angka3 = 0; // ini integer (nol)
+
+echo $angka1; // output: 5
+echo $angka2; // output: -10
+echo $angka3; // output: 0
+
+echo "<br></br>";
+
+$angkaDesimal1 = 3.14; // ini adalah float
+$angkaDesimal2 = -7.5; // ini juga float
+$angkaDesimal3 = 0.75; // ini juga float
+
+echo $angkaDesimal1; // output: 3.14
+echo $angkaDesimal2; // output: -7.5
+echo $angkaDesimal3; // output: 0.75
+
+echo "<br></br>";
+
+$numberOne = 13;
+$numberTwo = 5;
+$result = $numberOne / $numberTwo;
+
+echo $result;
+
+echo "<br></br>";
+
+$angkaIncrement = 10;
+$angkaIncrement++;
+echo $angkaIncrement;
+
+echo "<br></br>";
+
+$angkaDecrement = 10;
+$angkaDecrement--;
+echo $angkaDecrement;
