0795d9414341529456adedd86f0a9bd3233d2709
Hoerudin <hoerudinpermana30@gmail.com>
2024-11-12 17:05:04 +0700

    membuat template todo app

---
+++ management_file/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Todo App</h1>
+    <form action="">
+        <label for="">Daftar belanja hari ini</label><br>
+        <input type="text" name="todo" id="">
+        <input type="submit" value="Simpan">
+    </form>
+    <ul>
+        <li>
+            <input type="checkbox" name="todo" id="">
+            <label for="">Todo 1</label>
+            <a href="">hapus</a>
+        </li>
+        <li>
+            <input type="checkbox" name="todo" id="">
+            <label for="">Todo 2</label>
+            <a href="">hapus</a>
+        </li>
+        <li>
+            <input type="checkbox" name="todo" id="">
+            <label for="">Todo 3</label>
+            <a href="">hapus</a>
+        </li>
+    </ul>
+</body>
+</html>
