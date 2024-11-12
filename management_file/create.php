2e0c49bed3f71a0943a72bc274aaf063c679e6b5
Hoerudin <hoerudinpermana30@gmail.com>
2024-11-12 15:42:19 +0700

    belajar serialize dan JSON

--- management_file/create.php
+++ management_file/create.php
@@ -1,14 +1,23 @@
 <?php
-$anggota = [
+// $anggota = [
+//     ['nama' => 'Hoerudin', 'alamat' => 'Bogor'],
+//     ['nama' => 'Bambang', 'alamat' => 'Bandung'],
+//     ['nama' => 'Putri Nur', 'alamat' => 'Jakarta']
+// ];
+
+// $data = serialize($anggota);
+// file_put_contents('data.txt', $data);
+
+// $outuput = file_get_contents('data.txt');
+// $hasil = unserialize($outuput);
+// print_r($hasil);
+
+// JSON
+$karyawan = [
     ['nama' => 'Hoerudin', 'alamat' => 'Bogor'],
     ['nama' => 'Bambang', 'alamat' => 'Bandung'],
     ['nama' => 'Putri Nur', 'alamat' => 'Jakarta']
 ];
 
-$data = serialize($anggota);
-file_put_contents('data.txt', $data);
-
-$outuput = file_get_contents('data.txt');
-$hasil = unserialize($outuput);
-print_r($hasil);
-
+$data = json_encode($karyawan);
+file_put_contents('karyawan.json', $data);
